fcfe34a5df870dfb9d380e5e2cc957c92fd8f7fb
First-TimeCoder ! <xuminxia.no2@gmail.com>
2024-09-16 19:17:20 +0000

    created wordle

---
+++ wordle.py
@@ -0,0 +1,33 @@
+word = "great"
+
+#colors for printing
+default = '\033[0m'
+green = '\033[92m'
+yellow = '\033[33m'
+
+#function to generate hint with correct letter colors
+def generate_hint(guess):
+    color = default
+    hint = ""
+    for i in range(5):
+        if guess[i] == word[i]:
+            color = green
+        elif guess[i] in word:
+            color = yellow
+        else:
+            color = default
+        #adds letter ith correct color to hint word
+        hint = hint + color + guess[i] + default
+    return hint
+            
+
+def game_loop():
+    user_guess = ""
+    for i in range (6):
+        user_guess =  input("give a guess:")
+        print(generate_hint(user_guess))
+        if user_guess == word:
+            print("congratulations!!!")
+            break
+
+game_loop()
